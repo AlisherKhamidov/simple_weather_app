5325b1f54b59320c013a8e9cd0ae451d228e55d5
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-03-27 15:26:59 +0200

    fixed weather codes

--- index.js
+++ index.js
@@ -8,18 +8,34 @@ function getWeatherDescriptionByCode(code) {
   console.log(code);
   switch (code) {
     case 0: return "Clear sky";
-    case 1, 2, 3: return "Mainly clear, partly cloudy, and overcast";
-    case 45, 48: return "Fog and depositing rime fog";
-    case 51, 53, 55: return "Drizzle: Light, moderate, and dense intensity";
-    case 56, 57: return "Freezing Drizzle: Light and dense intensity";
-    case 61, 63, 65: return "Rain: Slight, moderate and heavy intensity";
-    case 66, 67: return "Freezing Rain: Light and heavy intensity";
-    case 71, 73, 75: return "Snow fall: Slight, moderate, and heavy intensity";
+    case 1: return "Mainly clear, partly cloudy, and overcast";
+    case 2: return "Mainly clear, partly cloudy, and overcast";
+    case 3: return "Mainly clear, partly cloudy, and overcast";
+    case 45: return "Fog and depositing rime fog";
+    case 48: return "Fog and depositing rime fog";
+    case 51: return "Drizzle: Light, moderate, and dense intensity";
+    case 53: return "Drizzle: Light, moderate, and dense intensity";
+    case 55: return "Drizzle: Light, moderate, and dense intensity";
+    case 56: return "Freezing Drizzle: Light and dense intensity";
+    case 57: return "Freezing Drizzle: Light and dense intensity";
+    case 61: return "Rain: Slight, moderate and heavy intensity";
+    case 63: return "Rain: Slight, moderate and heavy intensity";
+    case 65: return "Rain: Slight, moderate and heavy intensity";
+    case 66: return "Freezing Rain: Light and heavy intensity";
+    case 67: return "Freezing Rain: Light and heavy intensity";
+    case 71: return "Snow fall: Slight, moderate, and heavy intensity";
+    case 73: return "Snow fall: Slight, moderate, and heavy intensity";
+    case 75: return "Snow fall: Slight, moderate, and heavy intensity";
     case 77: return "Snow grains";
-    case 66, 67: return "Freezing Rain: Light and heavy intensity";
-    case 80, 81, 82: return "Rain showers: Slight, moderate, and violent";
-    case 85, 86: return "Thunderstorm: Slight or moderate";
-    case 96, 99: return "Thunderstorm with slight and heavy hail";
+    case 66: return "Freezing Rain: Light and heavy intensity";
+    case 67: return "Freezing Rain: Light and heavy intensity";
+    case 80: return "Rain showers: Slight, moderate, and violent";
+    case 81: return "Rain showers: Slight, moderate, and violent";
+    case 82: return "Rain showers: Slight, moderate, and violent";
+    case 85: return "Thunderstorm: Slight or moderate";
+    case 86: return "Thunderstorm: Slight or moderate";
+    case 96: return "Thunderstorm with slight and heavy hail";
+    case 99: return "Thunderstorm with slight and heavy hail";
     default: return "";
   }
 }
@@ -30,8 +46,8 @@ async function main() {
     .then(response => response)
     .catch(err => console.error(err));
 
-  const { latitude, longitude, city} = coordinatesResponseObj;
- 
+  const { latitude, longitude, city } = coordinatesResponseObj;
+
   const weatherResponseObj = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
     .then(response => response.json())
     .then(response => response)
